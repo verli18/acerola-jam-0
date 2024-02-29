f4f48d221cd09d2332f911fdbc5db173b10b6ea8
vebrun <verlidesouza@gmail.com>
2024-02-29 20:43:41 -0300

    fourth time trying to get git to work, hope it works!

---
+++ main.lua
@@ -0,0 +1,8 @@
+g = love.graphics
+k = love.keyboard
+m = love.mouse
+
+scale = 5
+love.window.setMode( 256*scale, 256*scale)
+renderCanvas = g.newCanvas(256,256)
+g.setDefaultFilter("nearest","nearest")
